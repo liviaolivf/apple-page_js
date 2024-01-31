7c90e9c90e37a066d4da8ed397fb526089cd53b8
Livia de Oliveira Faria <livof18@gmail.com>
2024-01-31 11:42:54 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,80 @@
+const imagemVisualizacao = document.getElementById('imagem-visualizacao');
+const tituloProduto = document.getElementById('titulo-produto');
+const nomeCor = document.getElementById('nome-cor-selecionada');
+const miniaturaImagem0 = document.getElementById('0-imagem-miniatura');
+const miniaturaImagem1 = document.getElementById('1-imagem-miniatura');
+const miniaturaImagem2 = document.getElementById('2-imagem-miniatura');
+
+const verdeCipreste = {
+    nome: 'verde-cipreste',
+    pasta: 'imagens-verde-cipreste'
+}
+
+const azulInverno = {
+    nome: 'azul-inverno',
+    pasta: 'imagens-azul-inverno'
+}
+
+const meiaNoite = {
+    nome: 'meia-noite',
+    pasta: 'imagens-meia-noite'
+}
+
+const estelar = {
+    nome: 'estelar',
+    pasta: 'imagens-estelar'
+}
+
+const rosaClaro = {
+    nome: "rosa-claro",
+    pasta: "imagens-rosa-claro"
+}
+
+const opcoesCores = [verdeCipreste, azulInverno, meiaNoite, estelar, rosaClaro];
+const opcoesTamanho = ['41 mm', '45 mm'];
+
+let imagemSelecionada = 1;
+let tamanhoSelecionado = 1;
+let corSelecionada= 1;
+
+function trocarImagem() {
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id;
+    imagemSelecionada = idOpcaoSelecionada.charAt(0);
+    imagemVisualizacao.src = './imagens/opcoes-cores/' + opcoesCores[corSelecionada].pasta + '/imagem-' + imagemSelecionada + '.jpeg'
+}
+
+function trocarTamanho() {
+    //atualizar variável de controle do tamanho
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-tamanho"]:checked').id;
+    tamanhoSelecionado = idOpcaoSelecionada.charAt(0);
+
+    //mudar o título do produto de acordo com a opção de tamanho selecionada
+    tituloProduto.innerText = "Pulseira loop esportiva " + opcoesCores[corSelecionada].nome + " para caixa de " + opcoesTamanh[tamanhoSelecionado];
+
+    //mudar o tamanho da imagem de acordo com a opção de tamanho selecionada
+    if (opcoesTamanho[tamanhoSelecionado] === '41 mm') {
+        imagemVisualizacao.classList.add('caixa-pequena');        
+    } else {
+        imagemVisualizacao.classList.remove('caixa-pequena');
+    }
+}
+
+function trocarCor() {
+    //atualizar cor selecionada
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-cor"]:checked').id;
+    corSelecionada = idOpcaoSelecionada.charAt(0);
+
+    //trocar titulo da página
+    tituloProduto.innerText = "Pulseira loop esportiva " + opcoesCores[corSelecionada].nome + " para caixa de " + opcoesTamanh[tamanhoSelecionado];
+    
+    //trocar nome da cor
+    nomeCor.innerText = "Cor - " + opcoesCores[corSelecionada].nome;
+    
+    //trocar imagens das miniaturas exibidas
+    miniaturaImagem0.src= './imagens/opcoes-cores/' + opcoesCores[corSelecionada].pasta + '/imagem-0.';
+    miniaturaImagem1.src= './imagens/opcoes-cores/' + opcoesCores[corSelecionada].pasta + '/imagem-1.';
+    miniaturaImagem2.src= './imagens/opcoes-cores/' + opcoesCores[corSelecionada].pasta + '/imagem-2.';
+
+    //trocar imagem de visualização
+    imagemVisualizacao.src = './imagens/opcoes-cores/' + opcoesCores[corSelecionada].pasta + '/imagem-' + imagemSelecionada + '.jpeg'
+}
